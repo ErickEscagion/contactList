36b07be799c776b006d132f4936dbe06594bec20
Erick Escagion <erickeescagion@gmail.com>
2021-01-20 14:10:39 -0300

    fix button "Adicionar contatos"

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -4,7 +4,7 @@ sap.ui.define([
   "sap/base/Log",
   "sap/ui/model/json/JSONModel",
   "sap/ui/thirdparty/jquery",
-], function(Controller, MessageBox, Log, JSONModel, jQuery) {
+], function(Controller, MessageBox, Log, JSONModel, jQuery, oDialog1) {
   "use strict";
 
   return Controller.extend("com.myorg.contactList.controller.MainView", {
@@ -24,14 +24,53 @@ sap.ui.define([
 		        {
 		            "Name": "Bob",
 		            "Telephone": "(19)97858-1112"
-		        }
+            },
+            {
+              "Name": "Paula",
+              "Telephone": "(13)93333-1112"
+            }
 		    ]
 		});
 		return oModel;
 	},
   
     onClickAddContactButton: function(oEvent){
-    	MessageBox.success('clicou no botão novo contato');
+      var oDialog1 = new sap.ui.commons.Dialog("Dialog", {
+        modal: true,
+        closed: function(oControlEvent){
+          sap.ui.getCore().getElementById('Dialog').destroy();
+        }
+      });
+      oDialog1.setTitle("Adicionar Contatos");
+      var oLayout = new sap.ui.commons.layout.MatrixLayout({
+        columns: 2,
+        width: "100%"
+        });               
+             var oLabel=new sap.ui.commons.Label({text: "Nome"});
+             var oTF= new sap.ui.commons.TextField("empName",{width: '200px'});
+             oLayout.createRow(oLabel, oTF);
+     
+             var oLabel=new sap.ui.commons.Label({text: "Telefone"});
+             var oTF=new sap.ui.commons.TextField("empTelephone",{width: '200px'});
+             oLayout.createRow(oLabel, oTF);
+
+            oDialog1.addContent(oLayout);
+            oDialog1.addButton(new sap.ui.commons.Button({text: "Salvar", press:function(){
+
+            var name    = sap.ui.getCore().getControl("empName").getValue(); 
+            var telephone     = sap.ui.getCore().getControl("empTelephone").getValue(); 
+            console.log(name);
+            console.log(telephone);
+
+            const newContact = {
+              "Name": name,
+              "Telephone": telephone
+            }
+            this.getModel().getProperty("ContactsCollection").push(newContact);
+            alert("SALVO COM SUCESSO");
+            
+            oDialog1.close();}}));
+            oDialog1.open();
     },
 
     onClickChangeContactButton: function(oEvent){
@@ -40,7 +79,7 @@ sap.ui.define([
 
     onClickDeleteContactButton: function(oEvent){
     	MessageBox.success('clicou no botão excluir contato');
-    },
+    }
 
   });
 });

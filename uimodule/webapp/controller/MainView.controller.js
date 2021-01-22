aa194512106d200fdeb42a36e09eceafc8791703
Erick Escagion <erickeescagion@gmail.com>
2021-01-22 12:55:37 -0300

    finish a project

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -3,108 +3,106 @@ sap.ui.define([
   "sap/base/Log",
   "sap/ui/model/json/JSONModel",
   "sap/ui/core/Core",
-	"sap/m/Dialog",
-	"sap/m/DialogType",
-	"sap/m/Button",
-	"sap/m/ButtonType",
-	"sap/m/Label",
-	"sap/m/MessageToast",
-	"sap/m/Text",
-	"sap/m/TextArea"
+  "sap/m/Dialog",
+  "sap/m/DialogType",
+  "sap/m/Button",
+  "sap/m/ButtonType",
+  "sap/m/Label",
+  "sap/m/MessageToast",
+  "sap/m/Text",
+  "sap/m/TextArea",
+  "sap/m/MessageBox",
 
-], function(Controller, Log, JSONModel, Core, Dialog, DialogType, Button, ButtonType, Label, MessageToast, Text, TextArea) {
+], function (Controller, Log, JSONModel, Core, Dialog, DialogType, Button, ButtonType, Label, MessageToast, Text, TextArea, MessageBox) {
   "use strict";
 
   return Controller.extend("com.myorg.contactList.controller.MainView", {
 
-    onInit: function() {
+    onInit: function () {
       var oJSONModel = this.initSampleDataModel();
-			this.getView().setModel(oJSONModel);
+      this.getView().setModel(oJSONModel);
     },
 
-    initSampleDataModel : function() {
-		var oModel = new JSONModel({
-		    "ContactsCollection":[
-		        {
-		          "Name": "erick",
-		          "Telephone": "(15)99818-1242"
-		        },
-		        {
-		          "Name": "Bob",
-		          "Telephone": "(19)97858-1112"
-            },
-            {
-              "Name": "Paula",
-              "Telephone": "(13)93333-1112"
-            }
-		    ]
-		});
-		return oModel;
-	},
-  
-  onClickAddContactButton: function () {
-    if (!this.oSubmitDialog) {
-      this.oSubmitDialog = new Dialog({
-        type: DialogType.Message,
-        title: "Novo Contato",
-        content: [
-          new Label({
-            text: "Nome",
-            labelFor: "submissionNameAdd"
-          }),
-          new TextArea("submissionNameAdd", {
-            width: "100%",
-            placeholder: "Adicione o nome (obrigatorio)",
-          }),
-          
-          new Label({
-            text: "Telefone",
-            labelFor: "submissionTelephoneAdd"
-          }),
-          new TextArea("submissionTelephoneAdd", {
-            width: "100%",
-            placeholder: "Adicione o Telefone (obrigatorio)",
-          })
-        ],
-        beginButton: new Button({
-          type: ButtonType.Emphasized,
-          text: "Salvar",
-          enabled: true,
-          press: function () {
-            let varName = Core.byId("submissionNameAdd").getValue();
-            let varTelephone = Core.byId("submissionTelephoneAdd").getValue();
-            
-            let newContact = {
-              "Name": varName,
-              "Telephone": varTelephone
-            };
-
-          let oModel = this.getView().getModel();
-          
-          oModel.oData.ContactsCollection.push(newContact);
-          
-          let newModel = new JSONModel(oModel.oData);
-
-          this.getView().setModel(newModel);
-
-          
-            MessageToast.show("Contato Salvo!!\nnome:" +varName+ "\ntel:"+ varTelephone);
-
-            this.oSubmitDialog.close();
-          }.bind(this)
-        }),
-        endButton: new Button({
-          text: "Cancelar",
-          press: function () {
-            this.oSubmitDialog.close();
-          }.bind(this)
-        })
+    initSampleDataModel: function () {
+      var oModel = new JSONModel({
+        "ContactsCollection": [
+          {
+            "Name": "erick",
+            "Telephone": "(15)99818-1242"
+          },
+          {
+            "Name": "Bob",
+            "Telephone": "(19)97858-1112"
+          },
+          {
+            "Name": "Paula",
+            "Telephone": "(13)93333-1112"
+          }
+        ]
       });
-    }
-    this.oSubmitDialog.open();
-  },
+      return oModel;
+    },
 
-    onClickChangeContactButton: function(oEvent){
+    onClickAddContactButton: function () {
+      if (!this.oSubmitDialog) {
+        this.oSubmitDialog = new Dialog({
+          type: DialogType.Message,
+          title: "Novo Contato",
+          content: [
+            new Label({
+              text: "Nome",
+              labelFor: "submissionNameAdd"
+            }),
+            new TextArea("submissionNameAdd", {
+              width: "100%",
+              placeholder: "Adicione o nome (obrigatorio)",
+            }),
+
+            new Label({
+              text: "Telefone",
+              labelFor: "submissionTelephoneAdd"
+            }),
+            new TextArea("submissionTelephoneAdd", {
+              width: "100%",
+              placeholder: "Adicione o Telefone (obrigatorio)",
+            })
+          ],
+          beginButton: new Button({
+            type: ButtonType.Emphasized,
+            text: "Salvar",
+            enabled: true,
+            press: function () {
+              let varName = Core.byId("submissionNameAdd").getValue();
+              let varTelephone = Core.byId("submissionTelephoneAdd").getValue();
+
+              let newContact = {
+                "Name": varName,
+                "Telephone": varTelephone
+              };
+
+              let oModel = this.getView().getModel();
+              oModel.oData.ContactsCollection.push(newContact);
+              let newModel = new JSONModel(oModel.oData);
+              this.getView().setModel(newModel);
+              MessageToast.show("Contato Salvo!!\nnome:" + varName + "\ntel:" + varTelephone);
+              this.oSubmitDialog.close();
+            }.bind(this)
+          }),
+          endButton: new Button({
+            text: "Cancelar",
+            press: function () {
+              this.oSubmitDialog.close();
+            }.bind(this)
+          })
+        });
+      } else {
+        Core.byId('submissionNameAdd').setValue('');
+        Core.byId('submissionTelephoneAdd').setValue('');
+      }
+      this.oSubmitDialog.open();
+    },
+
+    onClickChangeContactButton: function (oEvent) {
       if (!this.OChangeDialog) {
         this.OChangeDialog = new Dialog({
           type: DialogType.Message,
@@ -115,16 +113,14 @@ sap.ui.define([
               labelFor: "submissionNameChange"
             }),
             new TextArea("submissionNameChange", {
-              width: "100%",
-              placeholder: "carregar o nome",
+              width: "100%"
             }),
             new Label({
               text: "Telefone",
               labelFor: "submissionTelephoneChange"
             }),
             new TextArea("submissionTelephoneChange", {
-              width: "100%",
-              placeholder: "carregar o telefone"
+              width: "100%"
             })
           ],
           beginButton: new Button({
@@ -132,16 +128,27 @@ sap.ui.define([
             text: "Salvar",
             enabled: true,
             press: function () {
-              const varName = Core.byId("submissionNameChange").getValue();
-              const varTelephone = Core.byId("submissionTelephoneChange").getValue();
-    
-            /**logica para alterar o contato
-             * 
-             * 
-             */
-
-            MessageToast.show("Contato Alterado!!\nnome:" +varName+ "\ntel:"+ varTelephone);
-  
+              indices = this.getView().byId('Table').getSelectedIndices()
+              let newModel = new JSONModel();
+              let oldModel = this.getView().getModel().oData.ContactsCollection;
+              //define o atributo ContactsCollection como sendo um array, pra poder fazer push depois
+              newModel.oData.ContactsCollection = [];
+              for (let i = 0; i < oldModel.length; i++) {
+                //verifica se a linha atual é a que precisa excluir
+                if (!indices.includes(i)) {
+                  newModel.oData.ContactsCollection.push(oldModel[i]);
+                }
+                else {
+                  let newContact = {
+                    "Name": Core.byId('submissionNameChange').getValue(),
+                    "Telephone": Core.byId('submissionTelephoneChange').getValue()
+                  };
+                  newModel.oData.ContactsCollection.push(newContact);
+                }
+              }
+              this.getView().setModel(newModel);
+              this.getView().byId('Table').setSelectedIndex()
+              MessageToast.show("Contato Alterado!!");
               this.OChangeDialog.close();
             }.bind(this)
           }),
@@ -153,51 +160,66 @@ sap.ui.define([
           })
         });
       }
-  
+
+      let indices = this.getView().byId('Table').getSelectedIndices()
+      if (indices.length > 1) {
+        MessageBox.alert("SELECIONE APENAS UMA LINHA!!!");
+        return;
+      }
+      else if (indices.length === 0) {
+        MessageBox.alert("SELECIONE UMA LINHA!!!");
+        return;
+      }
+      let model = this.getView().getModel().oData.ContactsCollection;
+      let linhaSelecionada = model[indices[0]]
+      Core.byId('submissionNameChange').setValue(linhaSelecionada.Name);
+      Core.byId('submissionTelephoneChange').setValue(linhaSelecionada.Telephone);
       this.OChangeDialog.open();
     },
 
-    onClickDeleteContactButton: function(oEvent){
+    onClickDeleteContactButton: function (oEvent) {
       if (!this.confirmDeletion) {
-				this.confirmDeletion = new Dialog({
-					type: DialogType.Message,
-					title: "Excluir o contato?",
-					content: new Text({ text: "Essa ação e irreversivel!!" }),
-					beginButton: new Button({
-						type: ButtonType.Emphasized,
-						text: "Confirmar",
-						press: function () {
-            let key = this.getView().getModel().oData.ContactsCollection;
+        this.confirmDeletion = new Dialog({
+          type: DialogType.Message,
+          title: "Excluir o contato?",
+          content: new Text({ text: "Essa ação e irreversivel!!" }),
+          beginButton: new Button({
+            type: ButtonType.Emphasized,
+            text: "Confirmar",
+            press: function () {
+              let newModel = new JSONModel();
+              let oldModel = this.getView().getModel().oData.ContactsCollection;
+              //define o atributo ContactsCollection como sendo um array, pra poder fazer push depois
+              newModel.oData.ContactsCollection = [];
+              let indiceAExcluir = this.getView().byId('Table').getSelectedIndices();
+              for (let i = 0; i < oldModel.length; i++) {
+                //verifica se a linha atual é a que precisa excluir
+                if (!indiceAExcluir.includes(i) || !indiceAExcluir.length) {
+                  let newContact = {
+                    "Name": oldModel[i].Name,
+                    "Telephone": oldModel[i].Telephone
+                  };
+                  newModel.oData.ContactsCollection.push(newContact);
+                }
+              }
+              this.getView().setModel(newModel);
+              this.getView().byId('Table').setSelectedIndex()
+              MessageToast.show("Contato(s) Excluido(s)");
+              this.confirmDeletion.close();
+            }.bind(this)
+          }),
+          endButton: new Button({
+            text: "Cancelar",
+            press: function () {
+              this.confirmDeletion.close();
+            }.bind(this)
+          })
+        });
+      }
+      this.confirmDeletion.open();
+    },
 
-            //aria-rowindex
-debugger
-  /**   
-            let oModel = this.getView().getModel()
-            
-            oModel.oData.ContactsCollection.destroy();
-
-            let newModel = new JSONModel(oModel.oData);
-
-            this.getView().setModel(newModel);
-*/
-
-							MessageToast.show("Contato Excluido "+key);
-							this.confirmDeletion.close();
-						}.bind(this)
-					}),
-					endButton: new Button({
-						text: "Cancelar",
-						press: function () {
-							this.confirmDeletion.close();
-						}.bind(this)
-					})
-				});
-			}
-
-			this.confirmDeletion.open();
-		},
-
-    onClickViewTestButton: function(oEvent){
+    onClickViewTestButton: function (oEvent) {
       var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
       oRouter.navTo("TestView");
     }
